27411f2bfadde207a8a7bd0d498fbe78a7b67cf9
Mohamedboukerche22 <mohamedboukerche55@gmail.com>
2025-06-11 21:50:48 +0100

    Create ChefinhisOffice.cpp

---
+++ ChefinhisOffice.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int Q;
+	scanf("%d",&Q);
+	while(Q--)
+	{
+	    int w,f;
+	    scanf("%d%d",&w,&f);
+	    cout<<4*w+f;
+	    printf("\n");
+	}
+
+}
