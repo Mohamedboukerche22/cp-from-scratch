70e080f94785ed63f5e8a92c9b8640486a2bb0d4
Mohamedboukerche22 <mohamedboukerche55@gmail.com>
2025-06-11 21:35:53 +0100

    Create JanmanshandAssignments.cpp

---
+++ JanmanshandAssignments.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int t;
+	cin >>t;
+	while(t--)
+	{
+	    int n;
+	    cin >>n;
+	    n+=3;
+	    if(n > 10)
+	    {
+	        cout<<"NO\n";
+	    }
+	    else
+	    {
+	        cout<<"YES\n";
+	    }
+	}
+
+}
